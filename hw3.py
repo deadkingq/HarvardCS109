4f23cf5c7d14a7e3b3575c1106479d283c6d534e
deadkingq <deadkingq@users.noreply.github.com>
2016-07-22 13:44:51 -0400

    Add files via upload

--- hw3.py
+++ hw3.py
@@ -172,3 +172,151 @@ plt.plot(means.index, means['rtTopCriticsRating'], '-', label='Yearly Average')
 plt.legend(loc='lower left', frameon=False)
 plt.xlabel("Year")
 plt.ylabel("Average Score")
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cross_validation import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+
+def make_xy(critics, vectorizer=None):
+    if vectorizer is None:
+        vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(critics.quote)
+    X = X.tocsc()  # some versions of sklearn return COO format
+    Y = (critics.fresh == 'fresh').values.astype(np.int)
+    return X, Y
+
+X, Y = make_xy(critics)
+xtrain, xtest, ytrain, ytest = train_test_split(X, Y)
+clf = MultinomialNB().fit(xtrain, ytrain)
+
+print("Accuracy: %0.2f%%" % (100 * clf.score(xtest, ytest)))
+training_accuracy = clf.score(xtrain, ytrain)
+test_accuracy = clf.score(xtest, ytest)
+
+print("Accuracy on training data: %0.2f" % (training_accuracy))
+print("Accuracy on test data:     %0.2f" % (test_accuracy))
+
+def calibration_plot(clf, xtest, ytest):
+    prob = clf.predict_proba(xtest)[:, 1]
+    outcome = ytest
+    data = pd.DataFrame(dict(prob=prob, outcome=outcome))
+
+    #group outcomes into bins of similar probability
+    bins = np.linspace(0, 1, 20)
+    cuts = pd.cut(prob, bins)
+    binwidth = bins[1] - bins[0]
+
+    #freshness ratio and number of examples in each bin
+    cal = data.groupby(cuts).outcome.agg(['mean', 'count'])
+    cal['pmid'] = (bins[:-1] + bins[1:]) / 2
+    cal['sig'] = np.sqrt(cal.pmid * (1 - cal.pmid) / cal['count'])
+
+    #the calibration plot
+    ax = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    p = plt.errorbar(cal.pmid, cal['mean'], cal['sig'])
+    plt.plot(cal.pmid, cal.pmid, linestyle='--', lw=1, color='k')
+    plt.ylabel("Empirical P(Fresh)")
+    remove_border(ax)
+
+    #the distribution of P(fresh)
+    ax = plt.subplot2grid((3, 1), (2, 0), sharex=ax)
+
+    plt.bar(left=cal.pmid - binwidth / 2, height=cal['count'],
+            width=.95 * (bins[1] - bins[0]),
+            fc=p[0].get_color())
+
+    plt.xlabel("Predicted P(Fresh)")
+    remove_border()
+    plt.ylabel("Number")
+
+calibration_plot(clf, xtest, ytest)
+
+def log_likelihood(clf, x, y):
+    prob = clf.predict_log_proba(x)
+    rotten = y == 0
+    fresh = ~rotten
+    return prob[rotten, 0].sum() + prob[fresh, 1].sum()
+
+from sklearn.cross_validation import KFold
+
+def cv_score(clf, x, y, score_func):
+    result = 0
+    nfold = 5
+    for train, test in KFold(y.size, nfold): # split data into train/test groups, 5 times
+        clf.fit(x[train], y[train]) # fit
+        result += score_func(clf, x[test], y[test]) # evaluate score function on held-out data
+    return result / nfold # average
+
+#the grid of parameters to search over
+alphas = [0, .1, 1, 5, 10, 50]
+min_dfs = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+
+#Find the best value for alpha and min_df, and the best classifier
+best_alpha = None
+best_min_df = None
+max_loglike = -np.inf
+
+for alpha in alphas:
+    for min_df in min_dfs:
+        vectorizer = CountVectorizer(min_df = min_df)
+        X, Y = make_xy(critics, vectorizer)
+
+        #your code here
+        clf = MultinomialNB(alpha=alpha)
+        loglike = cv_score(clf, X, Y, log_likelihood)
+
+        if loglike > max_loglike:
+            max_loglike = loglike
+            best_alpha, best_min_df = alpha, min_df
+
+vectorizer = CountVectorizer(min_df=best_min_df)
+X, Y = make_xy(critics, vectorizer)
+xtrain, xtest, ytrain, ytest = train_test_split(X, Y)
+
+clf = MultinomialNB(alpha=best_alpha).fit(xtrain, ytrain)
+
+calibration_plot(clf, xtest, ytest)
+
+# Your code here. Print the accuracy on the test and training dataset
+training_accuracy = clf.score(xtrain, ytrain)
+test_accuracy = clf.score(xtest, ytest)
+
+words = np.array(vectorizer.get_feature_names())
+
+x = np.eye(xtest.shape[1])
+probs = clf.predict_log_proba(x)[:, 0]
+ind = np.argsort(probs)
+
+good_words = words[ind[:10]]
+bad_words = words[ind[-10:]]
+
+good_prob = probs[ind[:10]]
+bad_prob = probs[ind[-10:]]
+
+print "Good words\t     P(fresh | word)"
+for w, p in zip(good_words, good_prob):
+    print "%20s" % w, "%0.2f" % (1 - np.exp(p))
+
+print "Bad words\t     P(fresh | word)"
+for w, p in zip(bad_words, bad_prob):
+    print "%20s" % w, "%0.2f" % (1 - np.exp(p))
+
+x, y = make_xy(critics, vectorizer)
+
+prob = clf.predict_proba(x)[:, 0]
+predict = clf.predict(x)
+
+bad_rotten = np.argsort(prob[y == 0])[:5]
+bad_fresh = np.argsort(prob[y == 1])[-5:]
+
+print "Mis-predicted Rotten quotes"
+print '---------------------------'
+for row in bad_rotten:
+    print critics[y == 0].quote.irow(row)
+
+print "Mis-predicted Fresh quotes"
+print '--------------------------'
+for row in bad_fresh:
+    print critics[y == 1].quote.irow(row)
+
